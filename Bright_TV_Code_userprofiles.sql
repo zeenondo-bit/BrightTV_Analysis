fa6fcc622008ab082cdbc1a48a78596f8f3325c0
zeenondo-bit <zeenondo@gmail.com>
2026-07-12 13:44:33 +0000

    code update

--- Bright_TV_Code_userprofiles.sql
+++ Bright_TV_Code_userprofiles.sql
@@ -123,6 +123,124 @@ Select
         END AS Age_group
 FROM june_intake.bright_tv_casestudy.userprofiles;
 
+----------------------------------------------------------
+---combining both tables together
+CREATE OR REPLACE TEMPORARY TABLE user_profiles AS (
+SELECT UserID,
+    CASE
+        WHEN Province = 'None' THEN 'Uncategorised'
+        WHEN Province = ' ' THEN 'Uncategorised'
+        WHEN Province = 'other' THEN 'Uncategorised'
+        WHEN Province IS NULL THEN 'Uncategorised'
+        ELSE Province
+        END AS Region,
+
+        Age,
+     CASE
+        WHEN Age = 0 THEN 'Infants'
+        WHEN Age between 1 AND 12 THEN 'Kids'
+        WHEN Age between 13 AND 17 THEN 'Youth'
+        WHEN Age between 18 AND 35 THEN 'Young Adults'
+        WHEN Age between 36 AND 50 THEN 'Adults'
+        WHEN Age >50 AND Age <=60 THEN 'Elder'
+        WHEN Age >60 THEN 'Pensioner'
+        END AS Age_groups,
+
+    CASE
+        WHEN (Email IS NOT NULL) OR (Email = ' ') OR (Email NOT IN ('None','other')) THEN 1
+        ELSE 0
+        END AS Email_flag,
+
+    CASE
+        WHEN (`Social Media Handle` IS NOT NULL) OR (`Social Media Handle` = ' ') OR (`Social Media Handle` NOT IN ('None', 'other')) THEN 1
+        ELSE 0
+        END AS SM_flag,
+
+    CASE
+        WHEN Race = 'other' THEN 'None' 
+        WHEN Race = ' ' THEN 'None' 
+        ELSE Race 
+        END AS Race,
+
+      CASE
+        WHEN Gender = 'other' THEN 'None' 
+        WHEN Gender = ' ' THEN 'None' 
+        ELSE Gender
+        END AS Gender,
+
+FROM june_intake.bright_tv_casestudy.userprofiles);
+
+SELECT *
+FROM june_intake.bright_tv_casestudy.userprofiles;
+
+CREATE OR REPLACE TEMPORARY TABLE viewership AS (
+SELECT
+    COALESCE(UserID0,userid4) AS userid,
+    TO_CHAR(RecordDate2,'yyyyMM') AS month_id,
+    TO_DATE(RecordDate2) AS watch_date,
+    TIME(RecordDate2) AS watch_time,
+    TO_CHAR(RecordDate2,'DD') AS day_of_week,
+    DAYNAME(RecordDate2) AS day_name,
+
+    CASE
+        WHEN day_name IN ('Sat','Sun') THEN 'weekend'
+        ELSE 'weekday'
+        END AS day_classification,
+
+        MONTHNAME(RecordDate2) AS month_name,
+
+    CASE
+        WHEN Channel2 IN ('SaeSee','Sawsee') THEN 'SawSee'
+        WHEN Channel2 IN ('SuperSport Live Events','Live on SuperSport','Supersport Live Events','DStv Events1') THEN 'Live Events'
+        ELESE Channel2
+        END AS Tv_channel,
+
+    date_format(RecordDate2,'HH:mm:ss') AS watch_time,
+    CASE
+        WHEN watch_time BETWEEN '00:00:00' AND '05:59:59' THEN '01.Midnight'
+        WHEN watch_time BETWEEN '06:00:00' AND '11:59:59' THEN '02.Morning'
+        WHEN watch_time BETWEEN '12:00:00' AND '16:59:59' THEN '03.Afternoon'
+        WHEN watch_time BETWEEN '17:00:00' AND '23:59:59' THEN '04.Evening'
+        END AS time_of_day,
+    
+    date_format(Duartion 2,'HH:mm:ss') AS duration,
+    CASE
+        WHEN duration BETWEEN '00:05:00' AND '00:30:00' THEN '01.Low Usage:<30 min'
+        WHEN duration BETWEEN '00:30:01' AND '00:59:59' THEN '02.Mid Usage:<60 min'
+        WHEN duration > '00:59:59' THEN '03.High Usage:>60 min'
+        ELSE '04.No Usage'
+        END AS screen_time_bucket,
+
+    HOUR(RecordDate2)AS hour_of_day
+
+FROM june_intake.bright_tv_casestudy.viewership);
+
+SELECT *
+FROM viewership;
+
+SELECT Coalesce(A.userid,B.userid) AS sub_id,
+        month_id,
+        watch_date,
+        date_of_week,
+        day_name,
+        day_classification,
+        month_name,
+        Tv_channel,
+        time_of_day,
+        hour_of_day,
+        screen_time_bucket,
+        user_flag,
+        duration,
+        Region,
+        Age_groups,
+        Email_flag,
+        SM_flag,
+        Race,
+        Gender,
+FROM viewership AS A
+LEFT JOIN userprofils AS B
+ON A.userid = B.userid;
+
 
 
 
